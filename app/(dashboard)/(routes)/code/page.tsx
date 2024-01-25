a2df7e7b79bfd32dba93e865b14b0edb6ab3a0fd
aphuus <aphuus@utu.fi>
2024-01-25 23:44:42 +0200

    code generation ai api

---
+++ app/(dashboard)/(routes)/code/page.tsx
@@ -0,0 +1,149 @@
+'use client';
+
+import axios from 'axios';
+import * as z from 'zod';
+import { Code } from 'lucide-react';
+import { useForm } from 'react-hook-form';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { useRouter } from 'next/navigation';
+import { useState } from 'react';
+import ReactMarkdown from 'react-markdown';
+
+import Heading from '@/components/heading';
+import { Form, FormControl, FormField, FormItem, FormMessage } from '@/components/ui/form';
+import { Input } from '@/components/ui/input';
+import { Button } from '@/components/ui/button';
+import { Empty } from '@/components/empty';
+import { Loader } from '@/components/loader';
+import { cn } from '@/lib/utils';
+import UserAvatar from '@/components/user-avatar';
+import BotAvatar from '@/components/bot-avatar';
+
+import { formSchema } from './constants';
+
+interface ChatMessage {
+  role: 'system' | 'user' | 'assistant';
+  content: string;
+}
+
+const CodePage = () => {
+  const [messages, setMessages] = useState<ChatMessage[]>([]);
+  const router = useRouter();
+
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      prompt: ''
+    }
+  });
+
+  const isLoading = form.formState.isSubmitting;
+
+  const onSubmit = async (values: z.infer<typeof formSchema>) => {
+    try {
+      const userMessage: ChatMessage = {
+        role: 'user',
+        content: values.prompt
+      };
+      const newMessages = [...messages, userMessage];
+
+      const response = await axios.post('/api/code', { messages: newMessages });
+      setMessages((current) => [...current, userMessage, response.data]);
+
+      console.log(messages);
+
+      form.reset();
+    } catch (error: any) {
+      // TODO: Open Pro Modal
+      console.log(error);
+    } finally {
+      router.refresh();
+    }
+  };
+
+  return (
+    <div>
+      <Heading
+        title="Code Generation"
+        description="Generate code using natural language."
+        icon={Code}
+        iconColor="text-primary"
+        bgColor="bg-primary/10"
+      />
+      <div className="px-4 lg:px-8">
+        <div>
+          <Form {...form}>
+            <form
+              onSubmit={form.handleSubmit(onSubmit)}
+              className="rounded border w-full p-4 px-3 md:px-6 focus-within:shadow-sm grid grid-cols-12 gap-2"
+            >
+              <FormField
+                name="prompt"
+                control={form.control}
+                render={({ field }) => (
+                  <FormItem className="col-span-12 lg:col-span-10">
+                    <FormControl className="m-0 p-0">
+                      <Input
+                        className="border-0 outline-none focus-visible:ring-0 focus-visible:ring-transparent"
+                        placeholder="Enter the description of your code..."
+                        disabled={isLoading}
+                        {...field}
+                      />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <Button className="col-span-12 lg:col-span-2 w-full" disabled={isLoading}>
+                Generate
+              </Button>
+            </form>
+          </Form>
+        </div>
+        <div className="space-y-4 mt-4">
+          {isLoading && (
+            <div className="p-8 rounded-lg w-full flex items-center justify-center bgmut¨">
+              <Loader />
+            </div>
+          )}
+          {messages.length === 0 && !isLoading && (
+            <div>
+              <Empty label="There are no code generations!" />
+            </div>
+          )}
+          <div className="flex flex-col-reverse gap-y-4">
+            {messages.map((message, index) => (
+              <div
+                key={index}
+                className={cn(
+                  'p-6 w-full flex items-start gap-x-4 rounded',
+                  message.role === 'user' ? 'bg-white border border-black/10' : 'bg-muted'
+                )}
+              >
+                {message.role === 'user' ? <UserAvatar /> : <BotAvatar />}
+                <ReactMarkdown
+                  components={{
+                    pre: ({ node, ...props }) => (
+                      <div className="text-sm overflow-auto w-full my-2 bg-primary/10 p-6 rounded">
+                        <pre {...props} />
+                      </div>
+                    ),
+                    code: ({ node, ...props }) => (
+                      <code {...props} className="text-black rounded p-1" />
+                    ),
+                    p: ({ node, ...props }) => <p {...props} className="text-md font-medium" />
+                  }}
+                  className="text-sm overflow-hidden leading-7"
+                >
+                  {message.content || 'No content to display.'}
+                </ReactMarkdown>
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default CodePage;
---
+++ app/api/code/route.ts
@@ -0,0 +1,44 @@
+import { auth } from '@clerk/nextjs';
+import { NextResponse } from 'next/server';
+import OpenAI from 'openai';
+import { ChatCompletionMessageParam } from 'openai/resources/index.mjs';
+
+const openai = new OpenAI({
+  apiKey: process.env.OPENAI_API_KEY
+});
+
+const instructionMessage: ChatCompletionMessageParam = {
+  role: 'system',
+  content:
+    "Act as a code generator machine. You must only answer markdown code snippets back to me. I'll give you a prompt and you give me the code. You can use code comments for explanations."
+};
+
+export async function POST(req: Request) {
+  try {
+    const { userId } = auth();
+    const body = await req.json();
+    const { messages } = body;
+
+    if (!userId) {
+      return new NextResponse('Unauthorized', { status: 401 });
+    }
+
+    if (!openai.apiKey) {
+      return new NextResponse('OpenAI API Key Not Configured', { status: 500 });
+    }
+
+    if (!messages) {
+      return new NextResponse('Messages Not Found', { status: 400 });
+    }
+
+    const completion = await openai.chat.completions.create({
+      model: 'gpt-3.5-turbo',
+      messages: [instructionMessage, ...messages]
+    });
+
+    return NextResponse.json(completion.choices[0].message);
+  } catch (error) {
+    console.log('Code generation error: ', error);
+    return new NextResponse('Internal error', { status: 500 });
+  }
+}
